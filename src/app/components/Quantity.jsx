560fbda18d7d81ff448c80600ee49b1c747d195f
mubarishareef <mhareef88@gmail.com>
2023-10-19 14:28:50 +0530

    review session added

--- src/app/components/Quantity.jsx
+++ src/app/components/Quantity.jsx
@@ -4,16 +4,18 @@ import { useStateContext } from '../../../context/StateContext';
 import { AiOutlineMinus,AiOutlinePlus } from 'react-icons/ai'
 import {BiCommentDetail} from 'react-icons/bi'
 import Reviews from './Reviews';
+import Message from './Message';
 
 
 const Quantity = ({slug }) => {
     const [currSlug, setcurrSlug] = useState()
     const [showReview,setShowReview]=useState(false)
     useEffect(()=>setcurrSlug(slug),[])
-    const {qty,incQty,decQty,setqty}=useStateContext();
+    const {qty,incQty,decQty,setqty,user,viewMessage,showMessage}=useStateContext();
     if(currSlug!==slug){setqty(1)}
   return (<>
      {showReview && <Reviews setShowReview={setShowReview}/>}
+     {showMessage && <Message/>}
      <div className='quantity'>
          <h3>Quantity :</h3>
          <p className='quantity-desc'>
@@ -22,8 +24,8 @@ const Quantity = ({slug }) => {
              <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
          </p>
          <BiCommentDetail size={30} className='review-button' onClick={()=>{
-            setShowReview(true)
-            console.log('done');
+            if(user){setShowReview(true)}
+            else{viewMessage()}
          }}/>
          <p>Reviews (0)</p>
      </div>
